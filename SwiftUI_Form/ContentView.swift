3ffc43255ab89765876d148d1dd193a9e6340102
rbt200 <myesoap@mail.ru>
2020-04-21 13:25:08 -0700

    Finish the form

--- SwiftUI_Form/ContentView.swift
+++ SwiftUI_Form/ContentView.swift
@@ -9,8 +9,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var scheduled: Bool = false
+    @State private var manuallyEnabledTillTomorrow: Bool = false
+    @State private var colorTemperature: CGFloat = 0.5
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        NavigationView {
+            
+            Form {
+                
+                Section(header: Text("Night shift automatically shift colors. Night shift automatically shift colors. Night shift automatically shift colors. Night shift automatically shift colors. Night shift automatically shift colors.")
+                    .padding(5)
+                    .lineLimit(nil)
+                ) {
+                    Toggle(isOn: $scheduled) {
+                        Text("Scheduled")
+                    }
+                    
+                    HStack {
+                        VStack {
+                            Text("From")
+                            Text("To")
+                        }
+                        Spacer()
+                        NavigationLink(destination: Text("Scheduled Settings")) {
+                            VStack {
+                                Text("Sunset").foregroundColor(.blue)
+                                Text("Sunrise").foregroundColor(.blue)
+                            }
+                        }
+                    .fixedSize()
+                    }
+                }
+                
+                Section(header: Text("").padding(1)) {
+                    Toggle(isOn: $manuallyEnabledTillTomorrow) {
+                        Text("Manually enable untill tomorrow")
+                    }
+                }
+                
+                Section(header: Text("Color temperature").padding()) {
+                    HStack {
+                        Text("Less warm").offset(y: -10)
+                        Slider(value: $colorTemperature)
+                        Text("More warm").offset(y: -10)
+                    }
+                }
+                
+            }
+            
+        }
+        
     }
 }
 
